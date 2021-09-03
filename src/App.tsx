f2bd1cf5aece572a92bb93af46fdc10df26e2fcd
JustKahdri <joaco.rm512@gmail.com>
2021-09-02 21:13:15 -0300

    header layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,19 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <img src="" alt="" />
+        <div>
+          <h1>La próxima revolución en el intercambio de criptomonedas</h1>
+          <p>BataBit te ayuda navegar entre los diferentes precios y tendendcias.</p>
+          <a href="">Conoce nuestros planes <span>i</span></a>
+        </div>
       </header>
+      <main>
+        <section></section>
+      </main>
     </div>
   )
 }
